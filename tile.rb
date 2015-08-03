34fa48111a3dee0349cd28672b85586da26c65fb
Ari && Jordan <Ari+Jordan@appacademy.io>
2015-08-03 12:19:06 -0400

    Add instance variables with readers.

--- tile.rb
+++ tile.rb
@@ -1,2 +1,10 @@
 class Tile
+  attr_reader :has_bomb, :has_flag, :revealed, :neighbors
+
+  def initialize(bomb = false)
+    @has_bomb = bomb
+    @has_flag = false
+    @revealed = false
+    @neighbors = []
+  end
 end
