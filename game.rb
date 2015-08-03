dba4e553317d9a0f042d5010f6ce9edae1f934c7
Ari && Jordan <Ari+Jordan@appacademy.io>
2015-08-03 16:26:03 -0400

    Create skeleton for game play loop.

--- game.rb
+++ game.rb
@@ -3,7 +3,23 @@ require_relative 'board'
 class MinesweeperGame
   attr_reader :board
 
-  def initialize(board)
+  def initialize(board = Board.create_board)
     @board = board
+  end
+
+  def play
+    until board.over?
+      board.render
+      option, pos = prompt
+      option == 'f' ? board[pos].flag : board[pos].reveal
+    end
+
+    game_over
+  end
+
+  def prompt
+  end
+
+  def parse
   end
 end
